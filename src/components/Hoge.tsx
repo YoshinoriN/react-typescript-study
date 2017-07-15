bda6569425dd194c7a3b50c3dd5f3520a8017eef
YoshinoriN <nayo-msoft@outlook.jp>
2017-07-16 00:02:41 +0900

    Add Hoge.tsx

---
+++ src/components/Hoge.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export interface Hoge {}
+
+export const Hoge = (props: Hoge) => <h1> Hello hogege!!! </h1>
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,14 @@ import * as React from 'react';
 import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
+import { Hoge } from "./components/Hoge";
 
 ReactDOM.render(
   <Hello compiler="TypeScript" framework="React" />,
   document.getElementById("example")
+);
+
+ReactDOM.render(
+  <Hoge />,
+  document.getElementById("hoge")
 );
