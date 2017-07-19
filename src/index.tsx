03c2a9ce6d66fbec77033185e0ffe5231c672b95
YoshinoriN <nayo-msoft@outlook.jp>
2017-07-19 21:23:16 +0900

    Add TextBox.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
 import { Hoge } from "./components/Hoge";
+import { TextBox } from "./components/TextBox";
 
 ReactDOM.render(
   <Hello compiler="TypeScript" framework="React" />,
@@ -12,4 +13,9 @@ ReactDOM.render(
 ReactDOM.render(
   <Hoge />,
   document.getElementById("hoge")
+);
+
+ReactDOM.render(
+  <TextBox hello="Hello" />,
+  document.getElementById("textbox")
 );
